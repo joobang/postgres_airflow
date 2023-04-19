f80721eb735f7d1efe54c49980335e330739c9ae
dojoohwan <ehwnghks@gmail.com>
2023-04-19 22:08:36 +0900

    create: merge_upsert_unit_test.py 생성 및 테스트

--- src/dags/postgres_merge_upsert_unit_test.py
+++ src/dags/postgres_merge_upsert_unit_test.py
@@ -7,19 +7,6 @@ from utils.dag import DAG_DEFAULT_ARGS
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
-"""
-airflow Config 설정
-weight_rule : 작업간의 우선순위 결정.
-    - downstream : 후순위의 task일수록 우선순위가 낮아진다.
-    - upstream : 마지막에 가까운 task일수록 높은 우선순위.
-    - absolute : task가 모두 공평한 우선순위 할당.
-owner : 소유자 이름
-depends_on_past : True로 설정하면, 이전 날짜의 task 인스턴스 중에서 동일한 task 인스턴스가 실패한 경우 실행되지 않고 대기.
-retry_delay : 작업 실패 시 재시도 간격 설정값.
-retries : 작업 실패 시 최대 재시도 횟수.
-start_date : DAG 시작 날짜 설정.
-max_active_tis_per_dag : 하나의 DAG에서 동시에 실행할 수 있는 최대 작업 인스턴스 개수 설정.
-"""
 dag_default_args: Dict[str, Any] = {
     **DAG_DEFAULT_ARGS,
     'start_date': datetime(2022, 9, 1),
@@ -27,13 +14,14 @@ dag_default_args: Dict[str, Any] = {
 }
 
 with DAG(
-    'postgres_merge_upsert_ad_lineitem_test',
+    'postgres_merge_upsert_unit_test',
     default_args=dag_default_args,
-    description='This DAG merge-upserts the `ad_lineitem` table in the analytics_db with CDC logs from the source table',
+    description='This DAG that merge-upserts the `units` table in the analytics_db with CDC logs from the source table',
     schedule_interval='@once',
+    max_active_runs=1,
 ) as dag:
 
-    table = 'ad_lineitem'
+    table = 'unit'
 
     merge_upsert = PostgresOperator(
         task_id='merge_upsert',
@@ -50,11 +38,9 @@ with DAG(
 
             INSERT INTO {{ params.table }}
             (
-                SELECT DISTINCT ON (id)
+                SELECT
                     id,
-                    revenue_type,
-                    created_at,
-                    updated_at
+                    unit_type
                 FROM
                     {{ params.table }}_staging
                 WHERE 
@@ -64,7 +50,7 @@ with DAG(
             )
             ;
 
-            DELETE FROM
+           DELETE FROM
             {{ params.table }}_staging
             WHERE 
                 updated_at >= TIMESTAMP'{{ params.start_time }}'
